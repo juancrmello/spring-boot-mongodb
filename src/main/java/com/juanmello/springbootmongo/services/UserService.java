b0dd64d12960792457aa044e54bc74731b94a037
Juan Mello <juanribeirofx@gmail.com>
2019-09-24 03:52:05 -0300

    findById criado

--- src/main/java/com/juanmello/springbootmongo/services/UserService.java
+++ src/main/java/com/juanmello/springbootmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.juanmello.springbootmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.juanmello.springbootmongo.domain.User;
 import com.juanmello.springbootmongo.repository.UserRepository;
+import com.juanmello.springbootmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -15,8 +17,12 @@ public class UserService {
 	private UserRepository repo;
 	
 	public List<User> findAll() {
-		
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		
 	}
 }
